586874c5875cf6d9a63ea5848e6b60824a7361a2
HenrrySalinas <es.henrry@gmail.com>
2016-07-05 15:38:53 -0400

    regex constants and add extension to feature files

--- apiPivotalTest/src/main/java/com/fundacionjala/apiPivotalTest/Mapper.java
+++ apiPivotalTest/src/main/java/com/fundacionjala/apiPivotalTest/Mapper.java
@@ -1,14 +1,23 @@
 package com.fundacionjala.apiPivotalTest;
 
+import com.jayway.restassured.response.Response;
+
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.jayway.restassured.response.Response;
-
 import static com.jayway.restassured.path.json.JsonPath.from;
 
 public class Mapper {
+
+    private static final String REGEX_INSIDE_BRACKETS = "(?<=\\[)(.*?)(?=\\])";
+
+    private static final String REGEX_DOT = "\\.";
+
+    public static final String REGEX_BRACKETS = "[\\[\\]]";
+
+    public static final String REGEX_UNTIL_PROJECT = "^(\\/.*?\\/.*?\\/)";
+
     private Mapper() {
     }
 
@@ -17,23 +26,23 @@ public class Mapper {
     }
 
     public static String mapEndpoint(String endPoint, Map<String, Response> listResponses) {
-        Matcher matches = Pattern.compile("(?<=\\[)(.*?)(?=\\])").matcher(endPoint);
+        Matcher matches = Pattern.compile(REGEX_INSIDE_BRACKETS).matcher(endPoint);
         StringBuffer newEndPoint = new StringBuffer();
         String replaceParameter = "";
         while (matches.find()) {
-            String[] parametersParts = matches.group().split("\\.", 2);
+            String[] parametersParts = matches.group().split(REGEX_DOT, 2);
             String parameter = parametersParts[0];
             String field = parametersParts[1];
             replaceParameter = Mapper.getField(listResponses.get(parameter), field);
             matches.appendReplacement(newEndPoint, replaceParameter);
         }
         matches.appendTail(newEndPoint);
-        return newEndPoint.toString().replaceAll("[\\[\\]]", "");
+        return newEndPoint.toString().replaceAll(REGEX_BRACKETS, "");
     }
 
     public static String mapUrlToDeleteProject(String endPoint) {
         String projectUrlPart = "";
-        Matcher matches = Pattern.compile("^(\\/.*?\\/.*?\\/)").matcher(endPoint);
+        Matcher matches = Pattern.compile(REGEX_UNTIL_PROJECT).matcher(endPoint);
         while (matches.find()) {
             projectUrlPart = matches.group();
         }
