5eb8cdc4398235b73f2d069213783cbc14218c0d
HenrrySalinasJala <HenrrySalinas@fundacion-jala.org>
2016-07-04 20:54:44 -0400

    comments on story class cleaned

--- apiPivotalTest/src/test/java/com/fundacionjala/apiPivotalTest/cucumber/steps/stories/Story.java
+++ apiPivotalTest/src/test/java/com/fundacionjala/apiPivotalTest/cucumber/steps/stories/Story.java
@@ -2,25 +2,15 @@ package com.fundacionjala.apiPivotalTest.cucumber.steps.stories;
 
 import com.fundacionjala.apiPivotalTest.cucumber.steps.ApiResources;
 
-import org.json.simple.JSONObject;
-
 import cucumber.api.java.en.And;
 
 public class Story {
-
-    private JSONObject parameters;
 
     private ApiResources api;
 
     public Story(ApiResources api) {
         this.api = api;
     }
-
-//    @And("^I have the next parameters? to (?:create|update) a story:$")
-//    public void iHaveTheNextParametersToCreateAStory(Map<String, Object> storyData) {
-//        api.setParameters(storyData);
-//    }
-
 
     @And("^I have a story created$")
     public void iHaveAStoryCreated() {
